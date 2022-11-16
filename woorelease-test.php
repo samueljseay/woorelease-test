a436573dceb940cc5c9015bea0dec5ba20b0cb9d
roykho <roykho77@gmail.com>
2022-11-16 06:13:37 -0800

    Add main file

---
+++ woorelease-test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Woorelease test
+ * Version: 1.0.0
+ * Author: WooCommerce
+ * Author URI: https://woocommerce.com
+ * WC requires at least: 2.6
+ * WC tested up to: 7.1
+ * Tested up to: 6.0
+ * Copyright: © 2022 WooCommerce
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Woo: 18620:1a48b598b47a81559baadef15e320f11
+ *
+ * @package woorelease-test
+ */
